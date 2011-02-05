f57e369b304a5668efdf93048c3fc656fe94cb04
ctlink <ctlink@9566013c-90be-4a8c-a174-54c54c678395>
2011-02-05 21:34:50 +0000

    Add feature to FetchData set that finds the sent folder and includes it in the mail download.
    git-svn-id: svn+ssh://greensleeves.cs.vt.edu/home/cybergame/svn/trunk@11 9566013c-90be-4a8c-a174-54c54c678395

--- src/yao/gamelib/FetchDataset.java
+++ src/yao/gamelib/FetchDataset.java
@@ -1,6 +1,7 @@
 package yao.gamelib;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Properties;
 import java.util.logging.FileHandler;
@@ -9,6 +10,7 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 import javax.mail.AuthenticationFailedException;
+import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -68,7 +70,7 @@ public class FetchDataset {
      * Attempts to make connection to the server
      * @return true on successful connection, false otherwise
      */
-    private boolean CreateConnection() {
+    public boolean CreateConnection() {
         try {
             mImap.connect(mServer, mPort, mUsername, mPassword);
         } catch( AuthenticationFailedException e ) {
@@ -92,19 +94,45 @@ public class FetchDataset {
         inbox.open(Folder.READ_ONLY);
         return inbox;
     }
+    
+    public Folder GetSentFolder() throws MessagingException {
+        Folder DF;
+        Folder sent = null;
+        Folder[] list;
+        mLogger.log( Level.INFO, "Searching for sent folder");
+        DF = mImap.getDefaultFolder();
+        list = DF.list();
+        
+        list = DF.list("*sent*");
+        if( list.length == 0 ) {
+            list = DF.list("*Sent*");
+        }
+        if( list.length == 0 ) {
+            mLogger.log( Level.WARNING, "Could not find a sent mail folder.");
+            return null;
+        }
+        sent = list[0];
+        mLogger.log( Level.INFO, "Found sent folder: '"+sent.getName()+"'");
+        sent.open(Folder.READ_ONLY);
+        return sent;
+    }
 
     private Store PrepareMaildir() throws NoSuchProviderException {
         mProperties.put("mail.store.maildir.autocreatedir", "true");
-        String maildir_url = "maildir:" + mBaseDir + "/" + mUsername +"/Maildir";
+        String maildir_url = FetchDataset.MailDirDir(mBaseDir, mUsername);
         mLogger.log( Level.INFO, "Creating Maildir: "+ maildir_url);
         Store store = mSession.getStore(new URLName(maildir_url));
         return store;
     }
+    
+    public static String MailDirDir(String baseurl, String username) {
+        return "maildir:" + baseurl + "/" + username +"/Maildir";
+    }
 
     public boolean DownloadMail() {
         Store store;
-        Folder remote_inbox;
-        Folder local_inbox;
+        Folder remote_inbox, local_inbox;
+        Folder remote_sent, local_sent;
 
         mLogger.log( Level.INFO, "Establishing Connection...");
         if( !CreateConnection() ) {
@@ -117,40 +145,47 @@ public class FetchDataset {
         try {
             mLogger.log( Level.INFO, "Preparing local Maildir");
             store = PrepareMaildir();
-            mLogger.log( Level.INFO, "Getting remote inbox");
+            mLogger.log( Level.INFO, "Getting remote folders");
             remote_inbox = GetInbox();
+            remote_sent = GetSentFolder();
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
             mLogger.log( Level.SEVERE, "Could not create Maildir.");
             return false;
         } catch ( MessagingException e ) {
             e.printStackTrace();
-            mLogger.log( Level.SEVERE, "Could not retrieve remote inbox");
+            mLogger.log( Level.SEVERE, "Could not retrieve remote folders");
             return false;
         }
 
         try {
-            mLogger.log( Level.INFO, "Getting local INBOX folder");
+            mLogger.log( Level.INFO, "Getting local folders");
             local_inbox = store.getFolder("INBOX");
+            local_sent = store.getFolder("SENT");
         } catch( MessagingException e ) {
             e.printStackTrace();
-            mLogger.log( Level.SEVERE, "Could not retrieve local inbox");
+            mLogger.log( Level.SEVERE, "Could not retrieve local folders");
             return false;
         }
 
 
-        Message messages[];
+        Message inbox_messages[], sent_messages[];
         try {
             mLogger.log( Level.INFO, "Getting messages");
-            messages = remote_inbox.getMessages();
+            inbox_messages = remote_inbox.getMessages();
+            sent_messages = remote_sent.getMessages();
         } catch (MessagingException e) {
             e.printStackTrace();
             mLogger.log( Level.SEVERE, "Could not retrieve remote messages");
             return false;
         }
+        mLogger.log( Level.INFO, "Cleaning message list.");
+        inbox_messages = CleanMessages(inbox_messages);
+
         try {
             mLogger.log( Level.INFO, "Saving messages locally");
-            local_inbox.appendMessages(messages);
+            local_inbox.appendMessages(inbox_messages);
+            local_sent.appendMessages(sent_messages);
         } catch (MessagingException e) {
             e.printStackTrace();
             mLogger.log( Level.SEVERE, "Could not append local messages");
@@ -158,5 +193,45 @@ public class FetchDataset {
         }
         mLogger.log( Level.INFO, "Download operation complete.");
         return true;
+    }
+    
+    private Message[] CleanMessages(Message[] msgs) {
+        ArrayList<Message> cleaned = new ArrayList<Message>();
+        for( Message msg : msgs ) {
+            try {
+                Flags flags =  msg.getFlags();
+                if( flags.contains(Flags.Flag.SEEN) ) {
+                    
+                    cleaned.add(msg);
+                }
+            } catch (MessagingException e){
+                e.printStackTrace();
+                mLogger.log( Level.WARNING, "Flag retrieval failed");
+            }
+        }
+        Message[] cleaned_msgs = new Message[cleaned.size()];
+        return cleaned.toArray(cleaned_msgs);
+    }
+    
+    public void ListFolders() {
+        Folder DF;
+        Folder[] list;
+        mLogger.log( Level.INFO, "Listing Folders");
+        try {
+            DF = mImap.getDefaultFolder();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            list = DF.list();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return;
+        }
+        for(Folder f :list ) {
+            mLogger.log( Level.INFO, f.getName() );
+        }
     }
 }
