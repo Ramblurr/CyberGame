9d96af7f4df174d38803960199c1458478e84e9c
ctlink <ctlink@9566013c-90be-4a8c-a174-54c54c678395>
2011-02-28 14:29:14 +0000

    implement the generation of fake messages from a random range. currently implemented for FromWhenQuestion only.
    git-svn-id: svn+ssh://greensleeves.cs.vt.edu/home/cybergame/svn/trunk@33 9566013c-90be-4a8c-a174-54c54c678395

--- src/yao/gamelib/EmailQuestionFactory.java
+++ src/yao/gamelib/EmailQuestionFactory.java
@@ -1,21 +1,47 @@
 package yao.gamelib;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 
 public abstract class EmailQuestionFactory implements QuestionFactory {
-    private EmailStore mStore;
+    protected EmailStore mStore;
     
     public EmailQuestionFactory(EmailStore store) {
         mStore = store;
     }
 
+    @Override
     public abstract Question makeQuestion();
 
+    /**
+     * Choose a set of fake answers from the array of messages.
+     * 
+     * @param m
+     *            the message we are creating fake answers for
+     * @param msgs
+     *            the messages to choose fake answers from
+     * @return a string array containing the fake answers
+     * @throws MessagingException
+     */
+    public String[] makeFakeAnswers( Message m, Message[] msgs )
+            throws MessagingException {
+        return null;
+    }
+
     protected EmailQuestion setEmailDataInbox(EmailQuestion q) {
-        Message m = mStore.getNewMessageInbox();
-        
-        return setEmailData(m, q);
+        try {
+            Message m = mStore.getNewMessageInbox();
+
+            q.setFakeAnswers( makeFakeAnswers( m, getRandomRange( m ) ) );
+            return setEmailData(m, q);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
     
     protected EmailQuestion setEmailDataSent(EmailQuestion q) {
@@ -33,6 +59,61 @@ public abstract class EmailQuestionFactory implements QuestionFactory {
             e.printStackTrace();
         }
         return q; 
+    }
+
+    private Message[] getRandomRange( Message m ) throws MessagingException {
+        // first, generate the range
+        // from 1 to max_range_days
+        final int max_range_days = 30; // the range will not be greater than 30
+        Random randGen = new Random();
+        int range_days = 0;
+        do {
+            range_days = randGen.nextInt( max_range_days );
+        } while (range_days == 0); // we don't want 0 as the upper bound
+
+        System.out.println( "Using date range " + range_days );
+        // where in the range will the actual answer fall?
+        int date_position = randGen.nextInt( range_days );
+        System.out.println( "Using date position " + date_position );
+        // calculate the range in days
+        int days_after_date = range_days - date_position;
+        int days_before_date = date_position;
+
+        // convert those to actual dates based on the date of the message
+        Calendar c = Calendar.getInstance();
+        Date sent_date = m.getSentDate();
+
+        c.setTime( sent_date );
+        c.add( Calendar.DATE, -days_before_date );
+        Date begin_range = c.getTime();
+
+        c.clear();
+        c.setTime( sent_date );
+        c.add( Calendar.DATE, days_after_date );
+        Date end_range = c.getTime();
+
+        System.out.println( "Using date range " + begin_range.toString()
+                + " to " + end_range.toString() );
+
+        // get all messages within the range
+        Message[] msgs = mStore.getMessageInRange( "inbox", begin_range,
+                end_range );
+        // we need at least 4 messages in the range that aren't the actual
+        // message so we expand the range by 1 day on each end until
+        // we have at least 5 messages
+        while (msgs.length <= 5) {
+            c.clear();
+            c.setTime( begin_range );
+            c.add( Calendar.DATE, -1 );
+            begin_range = c.getTime();
+
+            c.clear();
+            c.setTime( end_range );
+            c.add( Calendar.DATE, 1 );
+            end_range = c.getTime();
+            msgs = mStore.getMessageInRange( "inbox", begin_range, end_range );
+        }
+        return msgs;
     }
 
 }
--- src/yao/gamelib/FromWhenFactory.java
+++ src/yao/gamelib/FromWhenFactory.java
@@ -1,5 +1,11 @@
 package yao.gamelib;
 
+import java.util.Date;
+import java.util.Random;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+
 //QuestionGenerator.registerType(Question.Type.FromWhen, new FromWhenFactory() );
 
 /**
@@ -11,10 +17,29 @@ public class FromWhenFactory extends EmailQuestionFactory {
         super(store);
     }
 
+    @Override
     public Question makeQuestion() {
         FromWhenQuestion q = new FromWhenQuestion();
-        
         return setEmailDataInbox(q);
+    }
+
+    @Override
+    public String[] makeFakeAnswers( Message m, Message[] msgs )
+            throws MessagingException {
+        Random randGen = new Random();
+        String[] answers = new String[4];
+        // randomly pick messages in the range to be fake answers
+        for ( int i = 0; i < answers.length; ++i ) {
+            int rand_index = randGen.nextInt( msgs.length );
+            Date ans_date = msgs[rand_index].getSentDate();
+            if( ans_date.equals( m.getSentDate() ) )
+                --i;
+            else {
+                System.out.println( "Using index: " + rand_index );
+                answers[i] = ans_date.toString();
+            }
+        }
+        return answers;
     }
 
 }
