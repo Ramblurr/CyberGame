cf6b7cf91fd5df512bd4e6a2f3db67c122bbf53c
Casey Link <unnamedrambler@gmail.com>
2011-03-21 01:42:50 -0400

    finish up code to display questions

--- src/yao/gameweb/QuizResource.java
+++ src/yao/gameweb/QuizResource.java
@@ -1,18 +1,28 @@
 package yao.gameweb;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.restlet.data.CharacterSet;
+import org.restlet.data.CookieSetting;
+import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
+import org.restlet.data.Status;
 import org.restlet.ext.freemarker.TemplateRepresentation;
 import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 
+import yao.gamelib.StoredQuestion;
+import yao.gameweb.util.AnswerStub;
+import yao.gameweb.util.Database;
+import yao.gameweb.util.QuestionStub;
 import yao.gameweb.util.TemplateUtil;
 import freemarker.template.Template;
 
@@ -33,7 +43,24 @@ public class QuizResource extends GameResource {
             if (username == null)
                 return null;
 
+            StoredQuestion[] questions = Database.getInstance().getQuestions(5);
+            ArrayList<QuestionStub> questionStubs = new ArrayList<QuestionStub>();
+            for (StoredQuestion q : questions) {
+                QuestionStub qstub = new QuestionStub();
+                qstub.text = q.getQuestion();
+                ArrayList<AnswerStub> answerStubs = new ArrayList<AnswerStub>();
+                for (AnswerStub stub : q.getFakeAnswerStubs()) {
+                    answerStubs.add(stub);
+                }
+                AnswerStub stub = new AnswerStub();
+                stub.id = q.getAnswerId();
+                stub.text = q.getAnswer();
+                answerStubs.add(stub);
+                qstub.answers = answerStubs;
+                questionStubs.add(qstub);
+            }
             pageData.put("username", username);
+            pageData.put("questions", questionStubs);
 
             Template thtml = TemplateUtil.getInstance().getTemplate("quiz.ftl");
             TemplateRepresentation rep = new TemplateRepresentation(thtml, pageData, MediaType.TEXT_HTML);
